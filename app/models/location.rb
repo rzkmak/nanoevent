716ccdbe5f95c50358d574be915fc07a5ca57a91
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-26 06:26:43 +0700

    Update: location model adding new spec validate length name should have minimum of 2

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,3 @@
 class Location < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Location, type: :model do
     it { is_expected.to validate_presence_of(:name) }
 
     it { is_expected.to validate_length_of(:name).is_at_most(50) }
+
+    it { is_expected.to validate_length_of(:name).is_at_least(2) }
   end
 end

abeba1ac1392b58141b47d3f3e62724a44306443
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 15:54:34 +0700

    Update: ticket model adding new spec validate quota should be presence

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :quota, presence: true
   belongs_to :event
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe Ticket, type: :model do
 
     it { is_expected.to validate_length_of(:name).is_at_least(2) }
   end
+
+  context 'ticket quota validation' do
+    it { is_expected.to validate_presence_of(:quota) }
+  end
 end

d9902c31c02654efa629fc5caeb71b675f432a34
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 16:34:52 +0700

    Update: customer model adding new spec validate length name should have maximum 30

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,3 @@
 class Customer < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 30}
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   context 'customer name validation' do
     it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
   end
 end

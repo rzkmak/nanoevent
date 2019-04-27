c877462b0a40813349447b36fae9bb4e19d4dc33
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 15:45:19 +0700

    Update: ticket model adding new spec name should be presence

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,4 @@
 class Ticket < ApplicationRecord
+  validates :name, presence: true
   belongs_to :event
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Ticket, type: :model do
   context 'ticket belong to specific event' do
     it { should belong_to(:event) }
   end
+
+  context 'ticket name validation' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end

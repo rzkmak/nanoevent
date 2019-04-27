eef37b5ba562c602c0eb4dc5e91938c6db742427
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 21:41:06 +0700

    Update: event controller add new spec for create new event with wrong format param should return bad request

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -19,4 +19,28 @@ RSpec.describe EventController, type: :controller do
       expect(response).to have_http_status :bad_request
     end
   end
+
+  context 'create wrong format params' do
+    before do
+      @location = create :location
+    end
+
+    let(:wrong_format_param) do
+      {
+        "name": 'Event A',
+        "organizer": 'Organizer A',
+        "capacity": 50,
+        "start_date": 'wrong date',
+        "end_date": '2013-02-02 01:00:00',
+        "location_id": @location.id
+      }
+    end
+
+    subject { post :create, params: { event: wrong_format_param } }
+
+    it 'should return bad request when having wrong format param' do
+      subject
+      expect(response).to have_http_status :bad_request
+    end
+  end
 end

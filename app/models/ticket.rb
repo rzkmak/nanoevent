e958c185fc413b90b73c0960316f9f126cb66fbb
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 15:59:37 +0700

    Update: ticket model adding new spec validate length description should have minimum 5

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,6 @@ class Ticket < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   validates :quota, presence: true
   validates :price, presence: true
-  validates :description, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
   belongs_to :event
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -25,5 +25,7 @@ RSpec.describe Ticket, type: :model do
     it { is_expected.to validate_presence_of(:description) }
 
     it { is_expected.to validate_length_of(:description).is_at_most(50) }
+
+    it { is_expected.to validate_length_of(:description).is_at_least(5) }
   end
 end

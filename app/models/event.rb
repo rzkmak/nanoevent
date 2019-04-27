cfa3f560bdbfe2724d732d9c0dccdfd726356372
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 15:02:20 +0700

    Update: event model adding new spec validate event name should be presence

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
   belongs_to :location
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Event, type: :model do
   context 'event belongs to specific location' do
     it { should belong_to(:location) }
   end
+
+  context 'event name validation' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end

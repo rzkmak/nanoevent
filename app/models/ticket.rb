b0853e33573b5df735971387a4387ad28c87415a
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 15:46:53 +0700

    Update: ticket model adding new spec length name should have maximum 20

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
   belongs_to :event
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Ticket, type: :model do
 
   context 'ticket name validation' do
     it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(20) }
   end
 end

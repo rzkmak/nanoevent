b95a2678806e588add9471b629c4cbf58b9c793b
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-27 17:30:23 +0700

    Update: purchase_ticket model adding new spec validate ticket should belong to ticket

--- spec/models/purchase_ticket_spec.rb
+++ spec/models/purchase_ticket_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseTicket, type: :model do
-  context 'event belongs to specific purchase' do
+  context 'purchase belongs to specific purchase' do
     it { should belong_to(:purchase) }
+  end
+
+  context 'ticket belongs to specific ticket' do
+    it { should belong_to(:ticket) }
   end
 end

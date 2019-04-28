616f09743d63afefa6dada812f62d880cf1e6b12
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-04-28 10:40:32 +0700

    Update: db seed to have initial dummy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Customer.create(name: 'Rizki Maulana', email: 'maulana@rizki.com')
+Customer.create([
+                  {
+                    name: 'Rizki Maulana',
+                    email: 'me@rizki.my.id'
+                  }])
+
+Location.create([
+                  {
+                    name: "Jalan Bangka",
+                    address: "Kebayoran Baru",
+                    city: "Jaksel",
+                    longitude: "0.0",
+                    latitude: "0.0"
+                  },
+                  {
+                    name: "Jalan Apel",
+                    address: "Lowokwaru",
+                    city: "Malang",
+                    longitude: "0.0",
+                    latitude: "0.0"
+                  }
+                ])
+
+Event.create(
+  [
+    name: "Peringatan Hari Guru",
+    organizer: "PGRI",
+    capacity: 50,
+    start_date: "2013-02-02T01:00:00.000Z",
+    end_date: "2013-02-02T01:00:00.000Z",
+    location_id: 1
+  ])
+
+Ticket.create(
+  [
+    "name": "Ticket Exclusive A",
+    "quota": 10,
+    "price": 50000,
+    "start_sell": "2013-02-02 01:00:00",
+    "end_sell": "2013-02-02 01:00:00",
+    "event_id": 1,
+    "description": "Exclusive ticket for special member"
+  ]
+)
